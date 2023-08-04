11f8e26eac82b0c09472caaa623fd9d9e0cb8b24
jeongjiwon <jeewonre@hufs.ac.kr>
2023-08-04 14:07:39 +0900

    project init

---
+++ src/main/java/com/darknights/devigation/DevigationApplication.java
@@ -0,0 +1,13 @@
+package com.darknights.devigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevigationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevigationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/darknights/devigation/DevigationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darknights.devigation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevigationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
