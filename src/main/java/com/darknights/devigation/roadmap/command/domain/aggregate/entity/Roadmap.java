fd6b5b41e0eb34a35632f93a3110afb521761404
JeonTaeHyeon <jth660084@gmail.com>
2023-08-08 19:14:35 +0900

    [Closes #12] Feat : roadmap context entity 작성

--- src/main/java/com/darknights/devigation/roadmap/command/domain/aggregate/entity/Roadmap.java
+++ src/main/java/com/darknights/devigation/roadmap/command/domain/aggregate/entity/Roadmap.java
@@ -3,14 +3,26 @@ package com.darknights.devigation.roadmap.command.domain.aggregate.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name="ROADMAP_TB")
 @NoArgsConstructor
 @Getter
 public class Roadmap {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @CreatedDate
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+
 }
