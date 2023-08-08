62c80b0fe8c29cfebfabfc02c3b5a7c0fefb479f
jeongjiwon <jeewonre@hufs.ac.kr>
2023-08-08 21:14:09 +0900

    [Closes #4] Feat : Create Application Service 클래스 생성

---
+++ src/test/java/com/darknights/devigation/member/command/application/service/CreateMemberServiceTest.java
@@ -0,0 +1,61 @@
+package com.darknights.devigation.member.command.application.service;
+
+import com.darknights.devigation.member.command.application.dto.CreateMemberDTO;
+import com.darknights.devigation.member.command.domain.aggregate.entity.enumType.PlatformEnum;
+import com.darknights.devigation.member.command.domain.aggregate.entity.enumType.Role;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class CreateMemberServiceTest {
+
+    @Autowired
+    private CreateMemberService createMemberService;
+
+
+    private static Stream<Arguments> getMemberInfo() {
+        return Stream.of(
+                Arguments.of(
+                        new CreateMemberDTO(
+                                1L,
+                                "name1",
+                                Role.MEMBER,
+                                "profileImage",
+                                "accessTokenTest",
+                                PlatformEnum.GITHUB
+                        )
+                ),
+                Arguments.of(
+                        new CreateMemberDTO(
+                                2L,
+                                "name2",
+                                Role.MEMBER,
+                                "profileImage",
+                                "accessTokenTest",
+                                PlatformEnum.GITHUB
+                        )
+                )
+        );
+    }
+    @DisplayName("사용자 생성 DTO를 통해 생성이 되는지 확인")
+    @ParameterizedTest
+    @MethodSource("getMemberInfo")
+    void create( CreateMemberDTO createMemberDTO) {
+
+        Assertions.assertDoesNotThrow(
+                () -> createMemberService.create(createMemberDTO)
+        );
+    }
+}
