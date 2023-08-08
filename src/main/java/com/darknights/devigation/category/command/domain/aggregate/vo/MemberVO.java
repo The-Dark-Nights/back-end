16e6962e35b6bd94f3f40d410b7cf0a110891f83
JeonTaeHyeon <jth660084@gmail.com>
2023-08-08 18:57:07 +0900

    [Closes #10]category context entity 작성

---
+++ src/main/java/com/darknights/devigation/category/command/domain/aggregate/vo/MemberVO.java
@@ -0,0 +1,15 @@
+package com.darknights.devigation.category.command.domain.aggregate.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Getter
+@Embeddable
+@NoArgsConstructor
+public class MemberVO {
+    @Column(name = "member_id")
+    private long id;
+}
