e1d9ac7d00466e45d12ded7cf75e5a549b026ff1
JeonTaeHyeon <jth660084@gmail.com>
2023-08-24 10:35:07 +0900

    [Closes #71], [Closes #73] Feat : Roadmap Category controller에서 post 요청 처리, json parsing

--- src/main/java/com/darknights/devigation/roadmap/command/application/dto/CreateEdgeDTO.java
+++ src/main/java/com/darknights/devigation/roadmap/command/application/dto/CreateEdgeDTO.java
@@ -4,13 +4,15 @@ package com.darknights.devigation.roadmap.command.application.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@ToString
 public class CreateEdgeDTO {
     private long roadmapId;
-    private String EdgeId;
+    private String edgeId;
     private long sourceCategory;
     private long targetCategory;
 }
